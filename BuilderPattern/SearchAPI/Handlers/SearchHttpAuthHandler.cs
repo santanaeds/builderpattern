af66c88bd05e33e9a408e1926eca912f1310e25e
Eduardo Santana <ludomi@microsoft.com>
2023-03-29 16:56:07 -0700

    Updated program class

---
+++ BuilderPattern/SearchAPI/Handlers/SearchHttpAuthHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using static SearchAPI.Models.Constants;
+
+namespace SearchAPI.Handlers
+{
+    public class SearchHttpAuthHandler : HttpClientHandler
+    {
+        private readonly ILogger<SearchHttpAuthHandler> _logger;
+        private readonly IConfiguration _config;
+        private readonly string _apiKey;
+
+        public SearchHttpAuthHandler(
+            ILogger<SearchHttpAuthHandler> logger,
+            IConfiguration config)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+            _apiKey =  ConfigKeys.SearchQueryApiKey;
+
+            if (string.IsNullOrWhiteSpace(_apiKey)) throw new ArgumentException("Value cannot be null, whitespace or empty.", nameof(_apiKey));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            try
+            {
+                request.Headers.Host = request.RequestUri.Host;
+                request.Headers.Add("api-key", _apiKey);
+
+                if (request.Content != null)
+                {
+                    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    request.Content.Headers.ContentLength = request.Content.ReadAsStringAsync(cancellationToken).GetAwaiter().GetResult().Length;
+                }
+
+                return await base.SendAsync(request, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error executing HTTP request.");
+                throw;
+            }
+        }
+    }
+}
--- BuilderPattern/SearchAPI/Program.cs
+++ BuilderPattern/SearchAPI/Program.cs
@@ -1,15 +1,52 @@
+using Refit;
+using SearchAPI.Builders;
+using SearchAPI.Handlers;
+using SearchAPI.Providers;
+using SearchAPI.Services;
+using System.Text.Json.Serialization;
+using static SearchAPI.Models.Constants;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddControllersAsServices()
+        .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+// Add builder
+builder.Services.AddSingleton<IDashboardResponseBuilder, DashboardResponseBuilder>();
+
+// Add services
+builder.Services.AddSingleton<ISearchApiServiceProvider>(sp =>
+{
+    var provider = sp.GetRequiredService<ISearchRefitDataProvider>();
+    var logger = sp.GetRequiredService<ILogger<SearchApiServiceProvider>>();
+    return new SearchApiServiceProvider(provider, logger);
+});
+builder.Services.AddSingleton<IDashboardService>(sp =>
+{
+    var provider = sp.GetRequiredService<ISearchApiServiceProvider>();
+    return new DashboardService(provider);
+});
+
+// Add Refit Search API
+builder.Services.AddRefitClient<ISearchRefitDataProvider>()
+    .ConfigurePrimaryHttpMessageHandler(sp =>
+    {
+        return new SearchHttpAuthHandler(
+            sp.GetRequiredService<ILogger<SearchHttpAuthHandler>>(),
+            sp.GetRequiredService<IConfiguration>());
+    })
+    .ConfigureHttpClient((sp, c) =>
+    {
+        c.BaseAddress = new Uri(ConfigKeys.SearchServiceEndpoint);
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
